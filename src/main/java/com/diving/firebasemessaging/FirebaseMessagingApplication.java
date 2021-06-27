698b6bd4a63ec035c8cea7dd699bd0b3fa470171
Kimyechan <vlvkcjswo7@naver.com>
2021-06-27 18:24:50 +0900

    Conf: Kafka Listener Config
    convert string to json

--- src/main/java/com/diving/firebasemessaging/FirebaseMessagingApplication.java
+++ src/main/java/com/diving/firebasemessaging/FirebaseMessagingApplication.java
@@ -1,13 +1,47 @@
 package com.diving.firebasemessaging;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
 
 @SpringBootApplication
 public class FirebaseMessagingApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:database.yml,"
+            + "classpath:kafka.yml,"
+            + "/home/ubuntu/config/project/pungdong/database.yml,"
+            + "/home/ubuntu/config/project/pungdong/kafka.yml,"
+            + "/home/ubuntu/config/project/pungdong/eureka.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(FirebaseMessagingApplication.class, args);
+        new SpringApplicationBuilder(FirebaseMessagingApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
+    @Bean
+    public RecordMessageConverter converter() {
+        return new StringJsonMessageConverter();
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+                                                                                 ConsumerFactory<Object, Object> kafkaConsumerFactory,
+                                                                                 KafkaTemplate<Object, Object> template) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+        factory.setErrorHandler(new SeekToCurrentErrorHandler(new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2)));
+        return factory;
+    }
 }
