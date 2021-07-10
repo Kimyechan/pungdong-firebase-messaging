f97556a57464e0373bd03703e0d33e3bc3184e09
Kimyechan <vlvkcjswo7@naver.com>
2021-07-10 15:31:42 +0900

    Conf: Test Config
    add h2 and kafka test config

--- src/test/java/com/diving/firebasemessaging/FirebaseMessagingApplicationTests.java
+++ src/test/java/com/diving/firebasemessaging/FirebaseMessagingApplicationTests.java
@@ -2,8 +2,10 @@ package com.diving.firebasemessaging;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FirebaseMessagingApplicationTests {
 
     @Test
